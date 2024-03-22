8fc5868e738da66fa6da5928334ec664b3ec1478
Cybershashank <shashanky485@gmail.com>
2024-03-22 22:21:24 +0530

    ..

--- backend/connection.js
+++ backend/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const url ="mongodb+srv://shashanky485:pradeepyadavANKIT@cluster0.jqcxcj5.mongodb.net/echostream?retryWrites=true&w=majority&appName=Cluster0"
+const url ="mongodb+srv://shashanky485:Shashanky485@cluster0.jqcxcj5.mongodb.net/echostream?retryWrites=true&w=majority&appName=Cluster0"
 
 mongoose.connect(url)
 .then((result) => {
