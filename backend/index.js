fbfa9e73123aba7c2876b250c3a458a23261fd3b
Hafsah1999 <hafsahnaseer233@gmail>
2024-04-19 00:15:25 -0700

    ...

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const podcastRouter = require('./routers/podcastRouter');
 const seriesRouter = require('./routers/seriesRouter');
 const userRouter = require('./routers/userRouter');
 const cors = require('cors');
+const utilRouter = require('./routers/Utils')
 
 const app = express();
 const port = 5000;
@@ -15,6 +16,9 @@ app.use('/artist', artistRouter);
 app.use('/podcast', podcastRouter);
 app.use('/series', seriesRouter);
 app.use('/user', userRouter);
+app.use('/util', utilRouter)
+
+app.use(express.static('./uploads'));
 
 app.get('/',(req,res) =>{
     res.send('response from express');
