00b6ae29bc1c3c7e14aa34504a9ba7ce7c18ae11
Hafsah1999 <hafsahnaseer233@gmail>
2024-04-19 23:48:37 -0700

    ...

--- frontend/src/app/(main)/layout.jsx
+++ frontend/src/app/(main)/layout.jsx
@@ -1,13 +1,18 @@
+"use client"
 import React from 'react'
 import Navbar from './navbar';
 import Footer from './footer';
+import App from 'next/app';
+import { AppProvider } from '@/app/Context/userContext';
 
 const MainLayout = ({ children }) => {
     return (
         <>
+     
             <Navbar />
             {children}
             <Footer />
+           
         </>
     )
 }
