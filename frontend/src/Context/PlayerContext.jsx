8fae46b83fa653805ae3b65b35b389de1676cad4
m-triple-m <triplem656@gmail.com>
2024-04-24 12:10:20 +0530

    fix player

--- frontend/src/Context/PlayerContext.jsx
+++ frontend/src/Context/PlayerContext.jsx
@@ -24,13 +24,14 @@ export const PlayerProvider = ({ children }) => {
             audioRef.current.pause();
             audioRef.current.currentTime = 0;
         }
-
     }
 
     return (
         <PlayerContext.Provider value={{ songPlaying, playerAction }}>
             {children}
             <audio className="hidden" ref={audioRef} />
+            {
+                songPlaying!==null && (
             <footer className="bg-gray-800 col-span-6  p-4 grid fixed w-full sticky bottom-0 grid-cols-3 gap-6">
                 <div className="flex items-center bg-black">
                     <img
@@ -40,10 +41,10 @@ export const PlayerProvider = ({ children }) => {
                     />
                     <div className="mr-4">
                         <div className="text-sm text-white text-line-clamp-1 font-light">
-                            Heaven
+                            {songPlaying.title}
                         </div>
                         <div className="text-xs text-gray-100 text-line-clamp-1">
-                            <span>Ludwig van Beethoven</span>
+                            <span>{songPlaying.artist.name}</span>
                         </div>
                     </div>
                     <div className="flex items-center">
@@ -91,13 +92,25 @@ export const PlayerProvider = ({ children }) => {
                             </svg>
                         </button>
                         <button className="w-8 h-8 border border-gray-300 rounded-full flex text-gray-100 mr-6">
-                            <svg
-                                className="fill-current h-5 w-5 m-auto"
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 20 20"
-                            >
-                                <path d="M5 4h3v12H5V4zm7 0h3v12h-3V4z" />
-                            </svg>
+                            {
+                                audioRef.current && audioRef.current.paused ? (
+                                    <svg
+                                        className="fill-current"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 20 20"
+                                    >
+                                        <path d="M5 4v12l10-6z" />
+                                    </svg>
+                                ) : (
+                                    <svg
+                                        className="fill-current"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 20 20"
+                                    >
+                                        <path d="M4 5v10l9-5-9-5zm10 0h2v10h-2V5z" />
+                                    </svg>
+                                )
+                            }
                         </button>
                         <button className="w-5 h-5 text-gray-100 mr-6">
                             <svg
@@ -119,15 +132,21 @@ export const PlayerProvider = ({ children }) => {
                         </button>
                     </div>
                     <div className="flex items-center">
-                        <span className="text-xs text-gray-100 font-light">4:18</span>
+                        <span className="text-xs text-gray-100 font-light">{
+                            audioRef.current && audioRef.current.currentTime
+                        }</span>
                         <div className="overflow-hidden relative flex-1 mx-2 rounded">
                             <div className="border-b-4 border-gray-400 rounded" />
                             <div className="absolute inset-x-0 top-0 -translate-x-48 border-b-4 border-gray-100 rounded transform hover:border-green-200" />
                         </div>
-                        <span className="text-xs text-gray-100 font-light">5:13</span>
+                        <span className="text-xs text-gray-100 font-light">
+                            {audioRef.current && audioRef.current.duration}
+                        </span>
                     </div>
                 </div>
             </footer>
+                )
+            }
         </PlayerContext.Provider>
     )
 }
