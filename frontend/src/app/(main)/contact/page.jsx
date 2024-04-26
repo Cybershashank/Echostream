d52c6510fba0a42ffd0882b051b0101f6ef49ec3
gauravttiwari <gauravtiwari96615@gmail.com>
2024-04-26 20:45:17 +0530

    add

--- frontend/src/app/(main)/contact/page.jsx
+++ frontend/src/app/(main)/contact/page.jsx
@@ -52,6 +52,7 @@ const Contact = () => {
 
   return (
     <>
+    
       <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
         <div
           className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
@@ -184,6 +185,7 @@ const Contact = () => {
 
         </form>
       </div>
+    
     </>
   )
 }
