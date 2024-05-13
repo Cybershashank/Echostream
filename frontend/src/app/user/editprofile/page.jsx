7abcf3296fe0de7df64d814bbb7258843a4d7a43
Hafsah1999 <hafsahnaseer233@gmail>
2024-05-13 09:31:23 +0530

    profile page updated

--- frontend/src/app/user/editprofile/page.jsx
+++ frontend/src/app/user/editprofile/page.jsx
@@ -1,27 +1,15 @@
-'use client';
-import { Formik, useFormik } from 'formik';
+'use client'
+import { Formik, useFormik } from 'formik'
 import React, { useState } from 'react'
-import toast from 'react-hot-toast';
+import toast from 'react-hot-toast'
 
-const editPofile = () => {
+
+
+const editProfile = () => {
 
   const [currentUser, setCurrentUser] = useState(JSON.parse(sessionStorage.getItem('user')))
 
 
-  const uploadProfileImage = (e) => {
-    const file = e.target.files[0];
-    const fd = new FormData();
-    fd.append('myfile', file)
-    fetch(`http://localhost:5000/util/uploadfile`, {
-      method: "POST",
-      body: fd,
-    }).then(res => {
-      if (res.status === 200) {
-        toast.success('Profile image uploaded successfully')
-        updateProfile({ avatar: file.name })
-      }
-    })
-  }
 
   const updateProfile = (data) => {
     fetch(`${process.env.NEXT_PUBLIC_API_URL}/user/update/${currentUser._id}`, {
@@ -33,6 +21,9 @@ const editPofile = () => {
     })
       .then(res => {
         console.log(res.status)
+        if (res.status === 200) {
+          toast.success('Profile image uploaded successfully')
+        }
         return res.json()
       })
       .then(data => {
@@ -45,150 +36,131 @@ const editPofile = () => {
   }
 
   return (
-    <>
+    <div>
       <>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
-          rel="stylesheet"
-        />
-        <style
-          dangerouslySetInnerHTML={{
-            __html:
-              "\n    body {\n        font-family: 'Plus Jakarta Sans', sans-serif;\n    }\n"
-          }}
-        />
-        <div className="bg-white w-full flex flex-col gap-5 px-3 md:px-16 lg:px-28 md:flex-row text-[#161931]">
+        {/* component */}
 
-          <main className="w-full min-h-screen py-1 md:w-2/3 lg:w-3/4">
-          {
-                    <Formik initialValues={currentUser} onSubmit={updateProfile}>
-                      {(updateProfile) => (
-            <div className="p-2 md:p-4">
-                  <div className="w-full px-6 pb-8 mt-8 sm:max-w-xl sm:rounded-lg">
-                <h2 className="pl-6 text-2xl font-bold sm:text-xl">Public Profile</h2>
-                <div className="grid max-w-2xl mx-auto mt-8">
-                  <div className="flex flex-col items-center space-y-5 sm:flex-row sm:space-y-0">
-                    <img
-                      alt="..."
-                      src={currentUser.avatar && `${process.env.NEXT_PUBLIC_API_URL}/${currentUser.avatar}`}
-                      className="shadow-xl rounded-full h-auto align-middle border-none absolute -m-16 -ml-20 lg:-ml-16 max-w-150-px"
-                    />
-                    <div className="flex flex-col space-y-5 sm:ml-8">
-                      <div className="text-center my-4">
-                        <label className='btn bg-white border hover:bg-slate-200' htmlFor='upload-image'>
-                          {" "} <i className='fas fa-pen'>&nbsp;Edit </i>
-                        </label>
-                        <input type="file" hidden onChange={uploadProfileImage} id="upload-image" />
-                      </div>
-                      <button
-                        type="button"
-                        className="py-3.5 px-7 text-base font-medium text-indigo-900 focus:outline-none bg-white rounded-lg border border-indigo-200 hover:bg-indigo-100 hover:text-[#202142] focus:z-10 focus:ring-4 focus:ring-indigo-200 "
-                      >
-                        Delete picture
-                      </button>
-                    </div>
-                  </div>
-                
+        <main className="profile-page">
 
-                        <div className="mx-auto grid max-w-screen-md gap-4 sm:grid-cols-2">
-                          <div>
-                            <label
-                              htmlFor="first-name"
-                              className="mb-2 inline-block text-sm text-gray-800 sm:text-base"
-                            >
-                              Name*
-                            </label>
-                            <input
-                              type='text'
-                              id="name"
-                              value={updateProfile.values.name}
-                              onChange={updateProfile.handleChange}
-                              className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"
-                            />
-                          </div>
-                          <div>
-                            <label
-                              htmlFor="last-name"
-                              className="mb-2 inline-block text-sm text-gray-800 sm:text-base"
-                            >
-                              Address*
-                            </label>
-                            <input
-                              type="text"
-                              id="address"
-                              className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"
-                              onChange={updateProfile.handleChange}
-                              value={updateProfile.values.address}
-                            />
-                          </div>
-                          <div>
-                            <label
-                              htmlFor="first-name"
-                              className="mb-2 inline-block text-sm text-gray-800 sm:text-base"
-                            >
-                              Email*
-                            </label>
-                            <input
-                              type='text'
-                              id="email"
-                              value={updateProfile.values.email}
-                              onChange={updateProfile.handleChange}
-                              className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"
-                            />
-                          </div>
-                          <div>
-                            <label
-                              htmlFor="last-name"
-                              className="mb-2 inline-block text-sm text-gray-800 sm:text-base"
-                            >
-                              Phone No.*
-                            </label>
-                            <input
-                              type='text'
-                              id="phone"
-                              value={updateProfile.values.phone}
-                              onChange={updateProfile.handleChange}
-                              className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"
-                            />
-                          </div>
+          <section className="relative  bg-blueGray-200">
 
-                          <div className="sm:col-span-2">
-                            <label
-                              htmlFor="message"
-                              className="mb-2 inline-block text-sm text-gray-800 sm:text-base"
-                            >
-                              Bio*
-                            </label>
-                            <textarea
-                              name="message"
-                              className="h-64 w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"
-                              defaultValue={""}
-                            />
-                          </div>
-                          <div className="flex items-center justify-between sm:col-span-2">
-                            <button type="submit" className="inline-block rounded-lg bg-indigo-500 px-8 py-3 text-center text-sm font-semibold text-white outline-none ring-indigo-300 transition duration-100 hover:bg-indigo-600 focus-visible:ring active:bg-indigo-700 md:text-base">
-                              Update
-                            </button>
+            <div className="bg-white   py-12 sm:py-8 lg:py-12">
+              <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
+                {/* text - start */}
+                <div className="mb-10 md:mb-16">
+                  <h2 className="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl">
+                    Update Profile
+                  </h2>
 
-                          </div>
+                </div>
+                {/* text - end */}
+                {/* form - start */}
+                {
+                  <Formik initialValues={currentUser} onSubmit={updateProfile}>
+                    {(updateProfile) => (
+
+
+                      <form onSubmit={updateProfile.handleSubmit} className="mx-auto grid max-w-screen-md gap-4 sm:grid-cols-2">
+                        <div>
+                          <label
+                            htmlFor="first-name"
+                            className="mb-2 inline-block text-sm text-gray-800 sm:text-base"
+                          >
+                            Name*
+                          </label>
+                          <input
+                            type='text'
+                            id="name"
+                            value={updateProfile.values.name}
+                            onChange={updateProfile.handleChange}
+                            className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"
+                          />
+                        </div>
+                        <div>
+                          <label
+                            htmlFor="last-name"
+                            className="mb-2 inline-block text-sm text-gray-800 sm:text-base"
+                          >
+                            Address*
+                          </label>
+                          <input
+                            type="text"
+                            id="address"
+                            className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"
+                            onChange={updateProfile.handleChange}
+                            value={updateProfile.values.address}
+                          />
+                        </div>
+                        <div>
+                          <label
+                            htmlFor="first-name"
+                            className="mb-2 inline-block text-sm text-gray-800 sm:text-base"
+                          >
+                            Email*
+                          </label>
+                          <input
+                            type='text'
+                            id="email"
+                            value={updateProfile.values.email}
+                            onChange={updateProfile.handleChange}
+                            className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"
+                          />
+                        </div>
+                        <div>
+                          <label
+                            htmlFor="last-name"
+                            className="mb-2 inline-block text-sm text-gray-800 sm:text-base"
+                          >
+                            Phone No.*
+                          </label>
+                          <input
+                            type='text'
+                            id="phone"
+                            value={updateProfile.values.phone}
+                            onChange={updateProfile.handleChange}
+                            className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"
+                          />
+                        </div>
+
+                        <div className="sm:col-span-2">
+                          <label
+                            htmlFor="message"
+                            className="mb-2 inline-block text-sm text-gray-800 sm:text-base"
+                          >
+                            Bio*
+                          </label>
+                          <textarea
+                            name="message"
+                            className="h-64 w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"
+                            defaultValue={""}
+                          />
+                        </div>
+                        <div className="flex items-center justify-between sm:col-span-2">
+                          <button type="submit" className="inline-block rounded-lg bg-indigo-500 px-8 py-3 text-center text-sm font-semibold text-white outline-none ring-indigo-300 transition duration-100 hover:bg-indigo-600 focus-visible:ring active:bg-indigo-700 md:text-base">
+                            Update
+                          </button>
 
                         </div>
-                 
-                </div>
-  
+
+                      </form>
+                    )}
+                  </Formik>
+                }
+                {/* form - end */}
               </div>
             </div>
-          )}
-          </Formik>
-        }
-          </main>
-        </div>
+
+
+
+
+
+          </section>
+        </main>
       </>
 
-    </>
+
+    </div>
   )
 }
 
-export default editPofile;
+export default editProfile;
--- frontend/src/app/user/user_profile/page.jsx
+++ frontend/src/app/user/user_profile/page.jsx
@@ -2,97 +2,123 @@
 import { useFormik } from 'formik';
 import Link from 'next/link';
 import React, { useState } from 'react'
+import toast from 'react-hot-toast';
 
 const UserProfile = () => {
 
-  const [currentUser, setCurrentUSer ] = useState(
+  const [currentUser, setCurrentUSer] = useState(
     JSON.parse(sessionStorage.getItem("user"))
-);
+  );
 
- const useForm = useFormik({
-  initialValues: currentUser, 
-  onSubmit : async (data) => {
-    console.log(data);
-  const res = await fetch(url + '/user/update/'+currentUser._id, {
-    method: 'PUT',
-    body: JSON.stringify(data),
-    headers: {
-      'Content-Type': 'application/json'
+  const useForm = useFormik({
+    initialValues: currentUser,
+    onSubmit: async (data) => {
+      console.log(data);
+      const res = await fetch(url + '/user/update/' + currentUser._id, {
+        method: 'PUT',
+        body: JSON.stringify(data),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      console.log(res.status);
+      const userData = await res.json();
+      console.log(userData);
+      setCurrentUSer(userData);
+      sessionStorage.setItem("user", JSON.stringify(userData));
     }
   });
-  console.log(res.status);
-  const userData = await res.json();
-  console.log(userData);
-  setCurrentUSer(userData);
-  sessionStorage.setItem("user", JSON.stringify(userData));
+
+  const updateProfile = (data) => {
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/user/update/${currentUser._id}`, {
+      method: 'PUT',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    })
+      .then(res => {
+        console.log(res.status)
+        if (res.status === 200) {
+          toast.success('Profile image uploaded successfully')
+        }
+        return res.json()
+      })
+      .then(data => {
+        console.log(data),
+          setCurrentUser(data)
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
- });
+
+  const uploadProfileImage = (e) => {
+    const file = e.target.files[0];
+    const fd = new FormData();
+    fd.append('myfile', file)
+    fetch(`http://localhost:5000/util/uploadfile`, {
+      method: "POST",
+      body: fd,
+    }).then(res => {
+      if (res.status === 200) {
+        toast.success('Profile image uploaded successfully')
+        updateProfile({ avatar: file.name })
+      }
+    })
+  }
+
+
   return (
     <>
-      
 
-          <section className="relative pt-40 pb-24">
+
+      <section className="relative pt-40 pb-24">
+        <img
+          src="https://pagedone.io/asset/uploads/1705473378.png"
+          alt="cover-image"
+          className="w-full absolute top-0 left-0 z-0 h-60"
+        />
+        <div className="w-full max-w-7xl mx-auto px-6 md:px-8">
+          <div className="flex items-center justify-center sm:justify-start relative z-10 mb-5">
             <img
-              src="https://pagedone.io/asset/uploads/1705473378.png"
-              alt="cover-image"
-              className="w-full absolute top-0 left-0 z-0 h-60"
+              src={currentUser.avatar && `${process.env.NEXT_PUBLIC_API_URL}/${currentUser.avatar}`}
+              alt="user-avatar-image"
+              className="border-4 border-solid border-white rounded-full"
             />
-            <div className="w-full max-w-7xl mx-auto px-6 md:px-8">
-              <div className="flex items-center justify-center sm:justify-start relative z-10 mb-5">
-                <img
-                  src="https://pagedone.io/asset/uploads/1705471668.png"
-                  alt="user-avatar-image"
-                  className="border-4 border-solid border-white rounded-full"
-                />
-              </div>
-              <div className="flex flex-col sm:flex-row max-sm:gap-5 items-center justify-between mb-5">
-                <div className="block">
-                  <h3 className="font-manrope font-bold text-4xl text-gray-900 mb-1">
-                   {currentUser.name}
-                  </h3>
-                  <p className="font-normal text-base leading-7 text-gray-500">
-                    {currentUser.email}
-                  </p>
-                </div>
-                <button className="py-3.5 px-5 flex rounded-full bg-indigo-600 items-center shadow-sm shadow-transparent transition-all duration-500 hover:bg-indigo-700">
-                  <svg
-                    width={20}
-                    height={20}
-                    viewBox="0 0 20 20"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M11.3011 8.69881L8.17808 11.8219M8.62402 12.5906L8.79264 12.8819C10.3882 15.6378 11.1859 17.0157 12.2575 16.9066C13.3291 16.7974 13.8326 15.2869 14.8397 12.2658L16.2842 7.93214C17.2041 5.17249 17.6641 3.79266 16.9357 3.0643C16.2073 2.33594 14.8275 2.79588 12.0679 3.71577L7.73416 5.16033C4.71311 6.16735 3.20259 6.67086 3.09342 7.74246C2.98425 8.81406 4.36221 9.61183 7.11813 11.2074L7.40938 11.376C7.79182 11.5974 7.98303 11.7081 8.13747 11.8625C8.29191 12.017 8.40261 12.2082 8.62402 12.5906Z"
-                      stroke="white"
-                      strokeWidth="1.6"
-                      strokeLinecap="round"
-                    />
-                  </svg>
-                  <span className="px-2 font-semibold text-base leading-7 text-white">
-                    Send Message
-                  </span>
-                </button>
-
-              </div>
-              <div className="flex flex-col lg:flex-row max-lg:gap-5 items-center justify-between py-0.5">
-                <div className="flex items-center gap-4">
-                  <Link href="http://localhost:3000/user/editprofile"><button className="py-3.5 px-5 rounded-full bg-indigo-600 text-white font-semibold text-base leading-7 shadow-sm shadow-transparent transition-all duration-500 hover:shadow-gray-100 hover:bg-indigo-700">
-                    Edit Profile
-                  </button></Link>
-                  <button className="py-3.5 px-5 rounded-full bg-indigo-50 text-indigo-600 font-semibold text-base leading-7 shadow-sm shadow-transparent transition-all duration-500 hover:bg-indigo-100">
-                    Settings
-                  </button>
-                </div>
+            <div className="w-full lg:w-4/12 px-4 lg:order-3  lg:self-center">
+              <div className="py-6 px-3 mt-32 sm:mt-0">
+                <label className='btn bg-white border hover:bg-slate-200' htmlFor='upload-image'>
+                  {" "} <i className='fas fa-pen'>&nbsp;Change</i>
+                </label>
+                <input type="file" hidden onChange={uploadProfileImage} id="upload-image" />
               </div>
             </div>
-          </section>
+          </div>
 
-          
-        </>
+          <div className="flex flex-col sm:flex-row max-sm:gap-5 mt-24 items-center justify-between mb-5">
+            <div className="block">
+              <h3 className="font-manrope font-bold text-4xl text-gray-900 mb-1">
+                {currentUser.name}
+              </h3>
+              <p className="font-normal text-base leading-7 text-gray-500">
+                {currentUser.email}
+              </p>
+            </div>
+            <Link href="http://localhost:3000/user/editprofile"><button className="py-3.5 px-5 rounded-full bg-indigo-600 text-white font-semibold text-base leading-7 shadow-sm shadow-transparent transition-all duration-500 hover:shadow-gray-100 hover:bg-indigo-700">
+              Edit Profile
+            </button></Link>
+
+          </div>
+
+        </div>
+      </section>
 
 
-      
+    </>
+
+
+
   )
 }
 
