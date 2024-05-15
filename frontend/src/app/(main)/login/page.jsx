3730bc22fc4da9937ed6f7c97fb837633da4649f
Cybershashank <shashanky485@gmail.com>
2024-05-15 22:01:15 +0530

    add

--- frontend/src/app/(main)/login/page.jsx
+++ frontend/src/app/(main)/login/page.jsx
@@ -112,7 +112,7 @@ const Login = () => {
           <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 className="text-center text-xl font-bold leading-tight tracking-tight text-gray-800 md:text-2xl dark:text-white">
-                USER LOGIN
+                LOGIN
               </h1>
               <form onSubmit={loginForm.handleSubmit} className="space-y-4 md:space-y-6" >
 
