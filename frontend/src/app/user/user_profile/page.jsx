f8592dfcae40836fcbefd964ee365bdabc2aa2a5
Hafsah1999 <hafsahnaseer233@gmail>
2024-05-15 12:46:05 +0530

    photo upload

--- frontend/src/app/user/user_profile/page.jsx
+++ frontend/src/app/user/user_profile/page.jsx
@@ -1,14 +1,13 @@
-
-import { useFormik } from "formik";
+'use client'
+import Link from "next/link";
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+
 
 const Profile = () => {
   const [currentUser, setCurrentUser] = useState(
     JSON.parse(sessionStorage.getItem("user"))
   );
 
-  const [profileData, setProfileData] = useState({});
 
   const getUserInfo = () => {
     fetch(`http://localhost:5000/user/getbyid`, {
@@ -20,7 +19,7 @@ const Profile = () => {
       })
       .then((data) => {
         console.log(data);
-        setProfileData(data);
+        setCurrentUser(data)
       })
       .catch((err) => {
         console.log(err);
